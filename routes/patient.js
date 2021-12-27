1f415cf36f4bcd9f769b0bd8b4c7fcbb1b824e29
Hrithik Prasad <65448486+Hrithikcse52@users.noreply.github.com>
2021-12-27 23:12:55 +0530

    Unhandled promise rejection error fix (#18)
    * Auth token bug resolved
    
    * feat: doctor-Patient-Filter
    
    filtered patients on doctor and created patients assigned to doctor
    
    * Random chanes
    
    * bug fix
    
    * fix: bugs on unhandled promiserejection !
    
    bugs on promise rejection

--- routes/patient.js
+++ routes/patient.js
@@ -1,6 +1,5 @@
 const { handleJWT } = require('../middleware/handleJWT');
 const patient = require('../databaseQueries/patient');
-
 const router = require('express').Router();
 
 router.post('/create', handleJWT, async (req, res) => {
@@ -30,52 +29,61 @@ router.post('/create', handleJWT, async (req, res) => {
     }
 });
 
-router.get('/:id', async (req, res) => {
-    console.log(req.params.id);
-    const { id } = req.params;
-    if (!id) {
-        res.status(400).send({ message: 'Send Proper Query' });
-        return;
-    }
-    const response = await patient.find_pt(id);
-    if (response.code === 401) {
-        res.status(400).send({ message: 'Something Went Wrong!' });
-        return;
-    }
-    console.log(response.data);
-    res.status(200).send(response.data);
-});
-
 router.get('/all', handleJWT, async (req, res) => {
-    const { user_id } = req;
-
     try {
+        const { user_id } = req;
+        console.log('User ID:', user_id);
+        console.log('Route Called!');
         const response = await patient.find_all({ pt_at: user_id });
         // console.log('Data', response);
         res.status(200).send(response);
     } catch (error) {
-        console.log(error);
+        console.log('Error at get all', error);
         res.status(400).send({ message: 'something went wrong !' });
     }
 });
 
 router.post('/all', handleJWT, async (req, res) => {
-    const { user_id } = req;
-    const { docId } = req.body;
-    if (!docId) {
-        return res.status(400).send({ message: 'Provide Doctor Id' });
-    }
-    const filter = {
-        pt_at: user_id,
-        docId,
-    };
     try {
+        console.log('called');
+        const { user_id } = req;
+        const { docId } = req.body;
+        if (!docId) {
+            return res.status(400).send({ message: 'Provide Doctor Id' });
+        }
+        const filter = {
+            pt_at: user_id,
+            docId,
+        };
+
         const resp = await patient.find_all(filter);
         // console.log('Data', res);
         res.status(200).send(resp);
     } catch (error) {
         console.log(error);
         res.status(400).send({ message: 'something went wrong !' });
+    }
+});
+
+router.get('/:id', async (req, res) => {
+    console.log('Called id ', req.params.id);
+
+    try {
+        const { id } = req.params;
+        if (!id) {
+            res.status(400).send({ message: 'Send Proper Query' });
+            return;
+        }
+        const response = await patient.find_pt(id);
+        if (response.code === 401) {
+            res.status(400).send({ message: 'Something Went Wrong!' });
+            return;
+        }
+        console.log(response.data);
+        res.status(200).send(response.data);
+    } catch (err) {
+        console.log('Error on id', err);
+        res.status(400).send({ message: "Coudn't make your request", err });
     }
 });
 
